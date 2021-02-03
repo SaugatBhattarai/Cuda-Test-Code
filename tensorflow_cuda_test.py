d7c3c987ad3f23a9d1b48139a834345b7a281af8
Saugat Bhattarai <the.saggitorres@gmail.com>
2021-02-03 11:21:41 +0545

    cuda and tensorflow test code added

---
+++ tensorflow_cuda_test.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+print("===============================================")
+print(tf.config.experimental.list_physical_devices('GPU'))
+
